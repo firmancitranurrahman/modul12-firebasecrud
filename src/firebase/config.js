74b6782d9efa654efb7966927e60479e3c8a7755
firmancitranurrahman <firmancitranurrahman@gmail.com>
2020-04-30 11:15:22 +0700

    finish

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyArSrD643cbMYlq7-J66TcxYeHgWxnVm28",
+    authDomain: "reactfirebaselogin-b8e6a.firebaseapp.com",
+    databaseURL: "https://reactfirebaselogin-b8e6a.firebaseio.com",
+    projectId: "reactfirebaselogin-b8e6a",
+    storageBucket: "reactfirebaselogin-b8e6a.appspot.com",
+    messagingSenderId: "432447425842",
+    appId: "1:432447425842:web:67ea9babfd7f3ea3585b2b",
+    measurementId: "G-99VK179BPR"
+  };
+
+  export default firebaseConfig;
